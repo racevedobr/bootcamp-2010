f77a95a76c6fea6579a6b6ef5cc20dd8f20ff42f
QA DevSoft <rodrigoacevedo@outlook.com>
2020-01-30 00:37:50 -0300

    Criado UserController e rota

--- modulo02/gobarber/src/database/migrations/20200129181631-create-users.js
+++ modulo02/gobarber/src/database/migrations/20200129181631-create-users.js
@@ -2,22 +2,22 @@ module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.createTable('users', {
             id: {
-               type: Sequelize.INTEGER,
-               allowNull: false,
-               autoIncrement: true,
-               primaryKey: true,
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
             },
             name: {
-                type: Sequelize.INTEGER,
+                type: Sequelize.STRING,
                 allowNull: false,
             },
             email: {
-                type: Sequelize.INTEGER,
+                type: Sequelize.STRING,
                 allowNull: false,
                 unique: true,
             },
             password_hash: {
-                type: Sequelize.INTEGER,
+                type: Sequelize.STRING,
                 allowNull: false,
             },
             provider: {
@@ -25,8 +25,14 @@ module.exports = {
                 defaultValue: false,
                 allowNull: false,
             },
-            created_at: {},
-            updated_at: {},
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
         });
     },
 
